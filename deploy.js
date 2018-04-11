d6b908d287f94efd623129775080aa085b925022
Carlos Jiménez <carlos.jimenez@guidesmiths.com>
2018-04-12 00:16:04 +0200

    adding console.log to print interface to be used in the lottery-react tutorial

--- deploy.js
+++ deploy.js
@@ -18,6 +18,7 @@ const deploy = async() => {
         .send({ gas: '1000000', from: accounts[0] });
 
     console.log('end')
+    console.log('interface', interface)
     console.log('Contract deployed to', result.options.address)
 };
 deploy();
